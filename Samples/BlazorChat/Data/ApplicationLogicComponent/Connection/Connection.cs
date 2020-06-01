323633cf6da8c559641f0e323619827f8b0c7bf0
Data Juggler <corby@datajuggler.com>
2020-06-01 18:36:33 -0500

    I am updating the first version with the SQL Scripts and some limited documentation.
    Video are more bug fixes are coming soon.

--- Samples/BlazorChat/Data/ApplicationLogicComponent/Connection/Connection.cs
+++ Samples/BlazorChat/Data/ApplicationLogicComponent/Connection/Connection.cs
@@ -14,8 +14,13 @@ namespace ApplicationLogicComponent.Connection
         /// <summary>
         /// Create a connection string and then create a system enivornment variable.
         /// Set the value of the System Environement Variable to the connectionstring.
+        /// A useful tool you might enjoy is a program called ConnectionStringBuilder,
+        /// which is Tool that comes with DataTier.Net, my open source project and preferred
+        /// c# data tier creator. DataTier.Net created the data tier including classes and stored procedures.
+        /// https://github.com/DataJuggler/DataTier.Net 
+        /// Look in the Tools folder for Connection String Builder. I use it often.
         /// </summary>
-        public const string Name = "MemberSampleConnection";
+        public const string Name = "BlazorChat";
         #endregion
 
         #region Properties
