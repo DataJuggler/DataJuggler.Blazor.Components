38ec09199b6aacfd61634a064178cd3317b33fd7
Data Juggler <corby@datajuggler.com>
2020-06-04 16:55:38 -0500

    Blazor Chat is now in its own repo. I will create another sample for this when I get a chance.

--- Samples/BlazorChat/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "<Pending>", Scope = "member", Target = "~M:BlazorChat.Pages.Index.OnAfterRenderAsync(System.Boolean)~System.Threading.Tasks.Task")]
-[assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "<Pending>", Scope = "member", Target = "~M:BlazorChat.Components.Join.ReceiveData(DataJuggler.Blazor.Components.Message)")]
