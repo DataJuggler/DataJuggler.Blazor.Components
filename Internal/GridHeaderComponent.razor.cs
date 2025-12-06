2db4faf280e67f7374c42ac422bf2015bd3b5a65
DataJuggler <corby@datajuggler.com>
2025-12-06 13:46:01 -0600

    Checking in before I start major modification to the grid.

---
+++ Internal/GridHeaderComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataJuggler.Blazor.Components;
+
+namespace DataJuggler.Blazor.Components.Internal
+{
+    internal partial class GridHeaderComponent : Grid
+    {
+    }
+}
